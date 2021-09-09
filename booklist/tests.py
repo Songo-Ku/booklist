ec85d205d1daf0024fe35e93069238684d1235c2
Songo-Ku <karczewski49@gmail.com>
2021-09-09 14:19:59 +0200

    added more basic test plus split into smaler and in one clas

--- booklist/tests.py
+++ booklist/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase, SimpleTestCase
+from django.test import TestCase, SimpleTestCase, Client
 from django.urls import reverse, resolve
 from rest_framework.test import APITestCase
 from .models import Book
@@ -8,61 +8,77 @@ from . import views
 
 class BooksViewsTestCase(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+        self.book_1 = Book.objects.create(
+            title='test123456',
+            authors_name='test_author',
+            published_date='2017-03-14',
+            language_book='pl',
+            link_book_cover='',
+            page_number=0,
+            isbn13_number=0
+        )
+        self.index_url = reverse('booklist:index')
+        self.filter_search_order_url = reverse('booklist:filter_search_order')
+        self.books_import_url = reverse('booklist:books_import')
+        self.book_detail_url = reverse('booklist:book_detail', args=[self.book_1.id, ])
+        self.book_update_url = reverse('booklist:book_update', args=[self.book_1.id, ])
+        self.book_delete_url = reverse('booklist:book_delete', args=[self.book_1.id, ])
+        self.book_add_url = reverse('booklist:book_add')
+        # ------------------
+        self.response_index = self.client.get(self.index_url)
+        self.response_filter_search_order = self.client.get(self.filter_search_order_url)
+        self.response_books_import = self.client.get(self.books_import_url)
+        self.response_book_detail = self.client.get(self.book_detail_url)
+        self.response_book_update = self.client.get(self.book_update_url)
+        self.response_book_delete = self.client.get(self.book_delete_url)
+        self.response_book_add = self.client.get(self.book_add_url)
+
+
+
+
     def test_index_view_status200(self):
-        url = reverse('booklist:index')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response_index.status_code, 200)
+
+    def test_index_view_proper_html(self):
+        self.assertTemplateUsed(self.response_index, 'booklist/index.html')
 
     def test_filter_search_order_view_status200(self):
-        url = reverse('booklist:filter_search_order')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response_filter_search_order.status_code, 200)
+
+    def test_index_view_proper_html(self):
+        self.assertTemplateUsed(self.response_filter_search_order, 'booklist/filter_search.html')
 
     def test_books_import_view_status200(self):
-        url = reverse('booklist:books_import')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response_books_import.status_code, 200)
+
+    def test_books_import_view_proper_html(self):
+        self.assertTemplateUsed(self.response_books_import, 'booklist/import_phrase.html')
 
     def test_book_detail_view_status200(self):
-        book = Book.objects.create(title='test123456',
-                                   authors_name='test_author',
-                                   published_date='2017-03-14',
-                                   language_book='pl',
-                                   link_book_cover='',
-                                   page_number=0,
-                                   isbn13_number=0)
-        url = reverse('booklist:book_detail', args=[book.id, ])
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response_book_detail.status_code, 200)
+
+    def test_book_detail_view_proper_html(self):
+        self.assertTemplateUsed(self.response_book_detail, 'booklist/book_detail.html')
 
     def test_book_update_view_status200(self):
-        book = Book.objects.create(title='test123456',
-                                   authors_name='test_author',
-                                   published_date='2017-03-14',
-                                   language_book='pl',
-                                   link_book_cover='',
-                                   page_number=0,
-                                   isbn13_number=0)
-        url = reverse('booklist:book_update', args=[book.id, ])
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response_book_update.status_code, 200)
+
+    def test_book_update_view_proper_html(self):
+        self.assertTemplateUsed(self.response_book_update, 'booklist/book_update_form.html')
 
     def test_book_delete_view_status200(self):
-        book = Book.objects.create(title='test123456',
-                                   authors_name='test_author',
-                                   published_date='2017-03-14',
-                                   language_book='pl',
-                                   link_book_cover='',
-                                   page_number=0,
-                                   isbn13_number=0)
-        url = reverse('booklist:book_delete', args=[book.id, ])
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response_book_delete.status_code, 200)
+
+    def test_book_delete_view_proper_html(self):
+        self.assertTemplateUsed(self.response_book_delete, 'booklist/delete_book.html')
 
     def test_book_add_view_status200(self):
-        url = reverse('booklist:book_add')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response_book_add.status_code, 200)
+
+    def test_book_add_view_proper_html(self):
+        self.assertTemplateUsed(self.response_book_add, 'booklist/book_create_form.html')
 
 
 class TestUrls(SimpleTestCase):
@@ -94,7 +110,6 @@ class TestUrls(SimpleTestCase):
     def test_book_update_url_is_resolved(self):
         url = reverse('booklist:book_update', args=[33, ])
         self.assertEquals(resolve(url).func.view_class, views.BookUpdateView)
-
 
 
 
